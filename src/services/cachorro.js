192cec4c2c02d5963a2588b22bde1cf0ecb360cb
Amanda <amandahaggerty6@gmail.com>
2023-10-05 19:30:52 -0300

    add services cachorro

---
+++ src/services/cachorro.js
@@ -0,0 +1,53 @@
+const RepositorioCachorros = require('../repositories/cachorro.js');
+const repositorio = new RepositorioCachorros();
+
+
+class ServicoCachorros {
+
+    VerificarCachorro(cachorro) {
+        if(!cachorro){
+            throw new Error('Não foi enviado um cachorro para cadastrar.');
+        } else if(!cachorro.nome){
+            throw new Error('Não foi enviado o nome do cachorro.');
+        } else if(!cachorro.raca){
+            throw new Error('Não foi enviado a raça do cachorro.');
+        } 
+        return true;
+    }
+
+    async ConsultarUm(idCachorro) {
+        if(isNaN(idCachorro)) {
+            throw new Error("Favor informar o ID apenas com número.");
+        } 
+        const resultadoId = repositorio.ConsultarUm(idCachorro)
+        console.log(resultadoId)
+        if(resultadoId == null) {
+            throw new Error("Esse ID não foi encontrado");
+        } else {
+            return resultadoId;
+        }
+    }
+
+    async ConsultarTodos() {
+        return repositorio.ConsultarTodos();
+    }
+
+    async Create(cachorro) {
+        this.VerificarCachorro(cachorro);
+        return repositorio.Create(cachorro);
+    }
+
+    async Update(idCachorro, cachorro) {
+        if(!idCachorro) {
+            throw new Error('Não foi enviado o identificador do cachorro para alterar.');
+        }
+        this.VerificarCachorro(cachorro);
+        return repositorio.Update(idCachorro, cachorro);
+    }
+
+    async Delete(idCachorro) {
+        return repositorio.Delete(idCachorro);
+    }
+}
+
+module.exports = ServicoCachorros
