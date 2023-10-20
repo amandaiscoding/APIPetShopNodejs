82f469b75a0c3ca8d102cef50ee9e7e154582912
Amanda <amandahaggerty6@gmail.com>
2023-10-20 10:04:56 -0300

    add authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,8 +3,8 @@ const config = require('../config')
 
 
 function authMiddleware(req, res, next) {
-    const token = req.headers['authorization']
-
+    const token = req.headers['authorization'] 
+    
     if(!token){
         return res.status(401).json({ message: 'Token não fornecido' })
     }    
